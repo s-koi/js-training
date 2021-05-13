afb7312f4e096438275a98fcbbe0bf78718f904d
s-koi <71995645+s-koi@users.noreply.github.com>
2021-05-13 11:26:37 +0900

    Add files via upload

--- src/pages/Lesson07/index.js
+++ src/pages/Lesson07/index.js
@@ -4,7 +4,38 @@ import Chart from "../../components/Chart07";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  for (const item of input) { // 日付を日本時間に変更
+    const d = new Date(`${item.createdAt} UTC`);
+    const year = d.getFullYear();
+    const month = `${d.getMonth() + 1}`.padStart(2, "0");
+    const date = `${d.getDate()}`.padStart(2, "0");
+    item.createdAt = `${year}-${month}-${date}`;
+  }
+  const dates = Array.from(new Set(input.map(({createdAt})=> createdAt)));
+  dates.sort();
+  const count = {tweet: {}, retweet: {}};
+  for (const d of dates) { // それぞれの日付のツイート&リツイートの数を初期化
+    count.tweet[d] = 0;
+    count.retweet[d] = 0;
+  }
+  for (const {createdAt, isRetweet} of input) { // ツイート&リツイートの数をカウント
+    if(isRetweet) {
+      count.retweet[createdAt] += 1;
+    }else{
+      count.tweet[createdAt] += 1;
+    }
+  }
+  return ["tweet", "retweet"].map((key) => { // データを表示
+    return {
+      id: key,
+      data: dates.map((d) => {
+        return {
+          x: d,
+          y: count[key][d],
+        };
+      }),
+    };
+  });
 };
 
 const Lesson = () => {
