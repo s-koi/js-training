dbc3170df31d18b1922f1eaa12842395ef7262e8
s-koi <71995645+s-koi@users.noreply.github.com>
2021-05-13 11:24:07 +0900

    Add files via upload

--- src/pages/Lesson02/index.js
+++ src/pages/Lesson02/index.js
@@ -4,7 +4,8 @@ import Chart from "../../components/Chart02";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  input.sort((tag1, tag2) => tag2.count-tag1.count); // 並び替え
+  return input.slice(0, 20); // 上位20件を表示
 };
 
 const Lesson = () => {
